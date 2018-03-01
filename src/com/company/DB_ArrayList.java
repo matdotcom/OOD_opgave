9898645bb56a88f889af8db2ac5e9a4778ff4a04
Captain <mads130f@edu.easj.dk>
2018-03-01 10:39:26 +0100

    arraylist

--- src/com/company/DB_ArrayList.java
+++ src/com/company/DB_ArrayList.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class ListOfAllKurser {
+public class DB_ArrayList {
     ArrayList<Kursus> arrayAfAlleKursus = new ArrayList<Kursus>();
 
 
