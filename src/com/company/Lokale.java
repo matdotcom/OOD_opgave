52a410f6664fe5e4a9ac0e06c735cf6e6ba22178
Andras Acs <acsandras@gmail.com>
2018-03-07 13:08:56 +0100

    Forbedringer

--- src/com/company/Lokale.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class Lokale {
-    private String navn;
-    private int nummer;
-}
--- src/com/company/Ugedage.java
+++
@@ -1,3 +0,0 @@
-package com.company;
-
-public enum Ugedage{mandag,tirsdag,onsdag,torsdag,fredag,lørdag}
